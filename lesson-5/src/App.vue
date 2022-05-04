c51bed76636dd737c7b4861ed79ce326e752c7c1
Stephen Strange <80538114+byStrange@users.noreply.github.com>
2022-05-04 19:54:34 +0500

    Header component

--- lesson-5/src/App.vue
+++ lesson-5/src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <Header />
 </template>
 
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: { Header },
+  name: 'App'
+}
+</script>
+
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
 </style>
