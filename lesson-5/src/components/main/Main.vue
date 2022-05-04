1b4e0eca8c58139d3c2e8eb689ce5af24e0b72e7
Stephen Strange <80538114+byStrange@users.noreply.github.com>
2022-05-04 20:35:51 +0500

    changes

---
+++ lesson-5/src/components/main/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <Background />
+  </div>
+</template>
+
+<script>
+
+import Background from "./Background";
+export default {
+  name: "Main",
+    components: { Background },
+};
+</script>
+
+<style lang="stylus">
+.main {
+  width: 100%;
+  height: calc(100vh - 75px);
+}
+</style>
