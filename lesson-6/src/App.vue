d987f4f00fe7eeb20bb27cf0367fda2622e435ae
Stephen Strange <80538114+byStrange@users.noreply.github.com>
2022-05-07 09:10:30 +0500

    +

--- lesson-6/src/App.vue
+++ lesson-6/src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    <router-view />
+    <div class="links">
+      <router-link :to="{ name: 'login' }">Login</router-link>
+      <router-link :to="{ name: 'signup' }">Signup</router-link>
+    </div>
+    <!-- bootstrap link -->
+  </div>
+  <!-- router view -->
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  name: "App",
+};
+</script>
--- lesson-6/src/router/index.js
+++ lesson-6/src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../components/Login.vue'
+import SignUp from '../components/SignUp.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
   }
 ]
 
